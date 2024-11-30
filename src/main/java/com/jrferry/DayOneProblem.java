06dcc77d8f22f6cce9931e5aef152301c914ec8c
jamesferry84 <jamesferry84@hotmail.com>
2024-11-30 18:47:56 +0000

    adding day 1

---
+++ src/main/java/com/jrferry/DayOneProblem.java
@@ -0,0 +1,8 @@
+package com.jrferry;
+
+public class DayOneProblem {
+
+    public static void main(String[] args) {
+        System.out.println("Day one class");
+    }
+}
---
+++ src/test/java/DayOneProblemTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class DayOneProblemTest {
+
+    @Test
+    public void testDayOneSolutions() {
+        System.out.println("testing works");
+    }
+}
